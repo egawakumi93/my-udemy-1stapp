2b884e691aa46d8d27175ec5f03f192649003215
kumi <k.egawa212@gmail.com>
2023-05-10 20:57:06 +0900

    いったん上げる

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const config = require('./config/dev')
+
+mongoose.connect(config.DB_URI)
+  .then(() => console.log('Connected!'));
+
+const app = express()
+
+app.get('/products', function(req, res){
+    res.json({'sucsess':true})
+})
+
+const PORT = process.env.PORT || '3001'
+app.listen(PORT, function() {
+    console.log('I am running!')
+})
